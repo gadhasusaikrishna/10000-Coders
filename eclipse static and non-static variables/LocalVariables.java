8b50a0932a5c609f4b73cec96a95aecfada110ed
gadhasusaikrishna <136802901+gadhasusaikrishna@users.noreply.github.com>
2025-03-12 16:09:02 +0530

    Update LocalVariables.java

---
+++ eclipse static and non-static variables/LocalVariables.java
@@ -0,0 +1,26 @@
+package demo;
+
+public class Local_variables {
+	public static void main (String[] args) {
+		byte b1=24;
+		short s1=23;
+		int i1=22;
+		long L1=4444;
+		float f1=55555555;
+		double d1=67899999;
+		char ch='$';
+		boolean bool = true;
+		String n1="saikrishna";
+		
+		System.out.println(b1);
+		System.out.println(s1);
+		System.out.println(i1);
+		System.out.println(L1);
+		System.out.println(d1);
+		System.out.println(ch);
+		System.out.println(bool);
+		System.out.println(n1);
+		System.out.println(f1);
+	}
+
+}
